5cb88e0c55a4edca0ad6a647a302e0bf2f337787
Boumaza Rayen <boumaza.rayen@outlook.fr>
2025-05-03 13:46:46 +0200

    v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 const isProd = process.env.NODE_ENV === "production";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  site: "https://rayen.dev.github.io/",
-  base: "/",
+  site: "https://rayenbou.github.io",
+  base: "/portfolio",
 });
